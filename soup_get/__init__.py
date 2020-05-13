9a29924259e9fbfc288f14b4d2b32c06a3be5dd8
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-05-13 07:41:46 -0700

    commit

--- soup_get/__init__.py
+++ soup_get/__init__.py
@@ -1,36 +1,9 @@
 import cached_url
-import time
-import random
-import yaml
+from bs4 import BeautifulSoup
 
-with open('credential') as f:
-	credential = yaml.load(f, Loader=yaml.FullLoader)
-
-class Timer(object):
-	def __init__(self):
-		self.reset()
-
-	def reset(self):
-		self.last_request = 0
-
-	def wait(self, wait):
-		if time.time() - self.last_request < wait:
-			print('wait', wait + self.last_request - time.time())
-			time.sleep(wait + self.last_request - time.time())
-		self.last_request = time.time()
+account_search_prefix = 'https://weixin.sogou.com/weixin?type=1&query='
 
 class SoupGet(object):
-	def __init__(self):
-		self.timer = Timer()
-		self.reset()
-
-	def reset(self):
-		self.num_requests = 0
-		self.timer.reset()
-
-	def getContent(self, url):
-		self.num_requests += 1
-		wait = min(random.random() * 10, self.num_requests / 3 * random.random())
-		self.timer.wait(wait)
-		return cached_url.get(url, 
-			headers = {'cookie': credential['cookie']})
+	def getAccountNewArticle(self, name):
+		soup = BeautifulSoup(cached_url.get(
+			account_search_prefix + name), 'html.parser')
