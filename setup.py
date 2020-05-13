54cad686109f3b522ffe5dd8a749db63793dc285
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-05-13 07:45:12 -0700

    commit

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import os
 import sys
 
 def kill():
-	os.system("ps aux | grep ython | grep weibo_aggregator | awk '{print $2}' | xargs kill -9")
+	os.system("ps aux | grep ython | grep weixin_aggregator | awk '{print $2}' | xargs kill -9")
 
 def setup():
 	kill()
 	if 'kill' in str(sys.argv):
 		return 
 	addtional_arg = ' '.join(sys.argv[1:])
-	command = 'python3 -u weibo_aggregator.py %s' % addtional_arg
+	command = 'python3 -u weixin_aggregator.py %s' % addtional_arg
 	if 'debug' in addtional_arg or 'skip' in addtional_arg or 'once' in addtional_arg:
 		os.system(command + ' test')
 	else:
