ac7e91105e829ca362c2ff40ead6710d86606a1d
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-05-13 08:16:08 -0700

    commit

--- soup_get/__init__.py
+++ soup_get/__init__.py
@@ -1,12 +1,23 @@
 import cached_url
 from bs4 import BeautifulSoup
+import yaml
 
 domain = 'https://weixin.sogou.com'
 account_search_prefix = '/weixin?type=1&query='
 
+with open('credential') as f:
+	credential = yaml.load(f, Loader=yaml.FullLoader)
+
 class SoupGet(object):
 	def getAccountNewArticle(self, name):
-		soup = BeautifulSoup(cached_url.get(
-			domain + account_search_prefix + name), 'html.parser')
+		content = cached_url.get(
+			domain + account_search_prefix + name)
+		soup = BeautifulSoup(content, 'html.parser')
 		item = soup.find('a', uigs='account_article_0')
 		return item and domain + item['href']
+
+	def getArticle(self, url):
+		return cached_url.get(
+			url,
+			force_cache=True,
+			headers = {'cookie': credential['cookie']})
--- weixin_aggregator.py
+++ weixin_aggregator.py
@@ -114,8 +114,9 @@ def loopImp():
 		url = sg.getAccountNewArticle(user)
 		if not url:
 			continue
-		print(url)
-		result = export_to_telegraph.export(url, force=True, throw_exception=True, force_cache=True)
+		sg.getArticle(url) # populate cache, because we need specific header
+		result = export_to_telegraph.export(
+			url, force_cache=True)
 		print(result)
 	print('loop finished')
 	command = 'git add . > /dev/null 2>&1 && git commit -m commit > /dev/null 2>&1 && git push -u -f > /dev/null 2>&1'
