7ee2b14a903afa5f1128d12087faba12df0af7c1
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-05-13 07:58:18 -0700

    commit

--- weixin_aggregator.py
+++ weixin_aggregator.py
@@ -111,7 +111,11 @@ def process(url):
 def loopImp():
 	db.reload()
 	for user in db.users.items:
-		print(sg.getAccountNewArticle(user))
+		url = sg.getAccountNewArticle(user)
+		if not url:
+			continue
+		result = export_to_telegraph.export(url, force_cache=True)
+		print(result)
 	print('loop finished')
 	command = 'git add . > /dev/null 2>&1 && git commit -m commit > /dev/null 2>&1 && git push -u -f > /dev/null 2>&1'
 	os.system(command)
